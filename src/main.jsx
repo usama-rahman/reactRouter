2e727767a52ae9d326ef48e4d1c1fa907b43469a
Usama Rahman <rahmanusama390@gmail.com>
2023-11-08 11:00:28 +0600

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Root from './Root';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
+import User from './Components/User/User';
 
 // Methode - 1
 
@@ -44,6 +45,7 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
     </Route>
   )
 );
