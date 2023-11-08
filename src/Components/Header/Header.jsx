2050cbf3ae3a38486281ce07f4363bed6d14f4a4
Usama Rahman <rahmanusama390@gmail.com>
2023-11-08 10:19:36 +0600

    Working

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
               <li>
                 <NavLink
                   to="/"
-                  className={isActive =>
+                  className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 ${
                       isActive ? 'text-orange-700' : 'text-gray-700'
                     } duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -50,7 +50,7 @@ export default function Header() {
               <li>
                 <NavLink
                   to="/about"
-                  className={isActive =>
+                  className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 ${
                       isActive ? 'text-orange-700' : 'text-gray-700'
                     } duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
